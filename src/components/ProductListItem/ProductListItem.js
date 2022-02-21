288fa045be39d2e2233f77cf7cf8a68d8f1667a8
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2022-02-21 20:34:19 -0300

    remove folder stories default

---
+++ src/components/ProductListItem/ProductListItem.js
@@ -0,0 +1,5 @@
+// ProductListItem.js
+import React from 'react';
+export default function ProductListItem() {
+  return <p>Hello from Product List Item!</p>;
+}
--- src/components/ProductListItem/ProductListItem.stories.js
+++ src/components/ProductListItem/ProductListItem.stories.js
@@ -1,4 +1,4 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import ProductListItem from './ProductListItem';
-storiesOf('Product List Item', module);
+import ProductListItem from './index';
+export default { title: 'ProductListItem' };
+export const standard = () => <ProductListItem />;
